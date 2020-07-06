dc139f4055dd628fcf6c2b157a12b6f30db0e6b3
Ramon-Ventura <ramonh_ventu@hotmail.com>
2020-07-05 21:45:16 -0600

    corregiendo error en sweetalert

--- public/js/modules/lecciones.js
+++ public/js/modules/lecciones.js
@@ -7,36 +7,38 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 // Obtener el nombre del boton para eliminar lecciones con DOM
-const btnEliminarLeccion = document.querySelector("#eliminar-leccion");
 
-// agregar evento
-btnEliminarLeccion.addEventListener("click", (e) => {
-  // capturar id de la leccion
-  const idLeccion = e.target.dataset.leccionId;
-  console.log(idLeccion);
-  Swal.fire({
-    title: "Estas seguro que deseas eliminar esta leccion?",
-    text: "Si eliminas esta leccion no sera posible recuperarla!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Eliminar lección",
-    cancelButtonText: "Cancelar",
-  }).then((result) => {
-    if (result.value) {
-      const url = `${location.origin}/leccion/${idLeccion}`;
-      console.log(url);
+//
+const btnEliminarLeccion = document.querySelectorAll("[id='eliminar-leccion']");
+for (var i = 0; i < btnEliminarLeccion.length; i++) {
+  btnEliminarLeccion[i].addEventListener("click", (e) => {
+    // capturar id de la leccion
+    const idLeccion = e.target.dataset.leccionId;
+    console.log(idLeccion);
+    Swal.fire({
+      title: "Estas seguro que deseas eliminar esta leccion?",
+      text: "Si eliminas esta leccion no sera posible recuperarla!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Eliminar lección",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.value) {
+        const url = `${location.origin}/leccion/${idLeccion}`;
+        console.log(url);
 
-      // implementar axios
-      axios
-        .delete(url, { params: { id: idLeccion } })
-        .then(function (response) {
-          Swal.fire("Eliminado!", "La leccion se ha eliminado.", "success");
-        })
-        .catch(() => {
-          Swal.fire("Error!");
-        });
-    }
+        // implementar axios
+        axios
+          .delete(url, { params: { id: idLeccion } })
+          .then(function (response) {
+            Swal.fire("Eliminado!", "La leccion se ha eliminado.", "success");
+          })
+          .catch(() => {
+            Swal.fire("Error!");
+          });
+      }
+    });
   });
-});
+}
